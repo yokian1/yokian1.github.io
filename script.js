16e967e7a6afa0d7a677b32787577dafddd15dae
Fernando Junco <fernandoojunco@hotmail.com>
2023-05-04 19:10:02 -0400

    una mas

--- script.js
+++ script.js
@@ -1,37 +1,62 @@
-const carouselSlide = document.querySelector('.carousel-slide');
-const carouselImages = document.querySelectorAll('.carousel-slide img');
 
-const prevBtn = document.querySelector('.carousel-prev');
-const nextBtn = document.querySelector('.carousel-next');
+ /////parte para que deje de darle al next al llegar al final de los carrousel
 
-let counter = 1;
+
+  const carouselSlide = document.querySelector(".carousel-slide");
+const carouselImages = document.querySelectorAll(".carousel-slide img");
+
+// Botones
+const prevBtn = document.querySelector("#prevBtn");
+const nextBtn = document.querySelector("#nextBtn");
+
+// Contador
+let counter = 0;
+
+// Tamaño de las imagenes
 const size = carouselImages[0].clientWidth;
 
-carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+// Moviendo a la primera imagen
+carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
 
-nextBtn.addEventListener('click', () => {
-  if (counter >= carouselImages.length - 1) return;
-  carouselSlide.style.transition = 'transform 0.4s ease-in-out';
+// Boton "Next"
+nextBtn.addEventListener("click", () => {
+  if (counter >= carouselImages.length - 1) return; // Verificar si es el último elemento
+  carouselSlide.style.transition = "transform 0.4s ease-in-out";
   counter++;
-  carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+  carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
 });
 
-prevBtn.addEventListener('click', () => {
-  if (counter <= 0) return;
-  carouselSlide.style.transition = 'transform 0.4s ease-in-out';
+// Boton "Prev"
+prevBtn.addEventListener("click", () => {
+  if (counter <= 0) return; // Verificar si es el primer elemento
+  carouselSlide.style.transition = "transform 0.4s ease-in-out";
   counter--;
-  carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+  carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
 });
 
-carouselSlide.addEventListener('transitionend', () => {
-  if (carouselImages[counter].id === 'last-clone') {
-    carouselSlide.style.transition = 'none';
+// Resetear al principio al llegar al último elemento
+carouselSlide.addEventListener("transitionend", () => {
+  if (carouselImages[counter].id === "lastClone") {
+    carouselSlide.style.transition = "none";
     counter = carouselImages.length - 2;
-    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
   }
-  if (carouselImages[counter].id === 'first-clone') {
-    carouselSlide.style.transition = 'none';
+  if (carouselImages[counter].id === "firstClone") {
+    carouselSlide.style.transition = "none";
     counter = carouselImages.length - counter;
-    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
+  }
+
+  // Habilitar/deshabilitar botones
+  if (counter >= carouselImages.length - 1) {
+    nextBtn.disabled = true;
+  } else if (counter <= 0) {
+    prevBtn.disabled = true;
+  } else {
+    nextBtn.disabled = false;
+    prevBtn.disabled = false;
   }
 });
+
+
+
